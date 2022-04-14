d11ff49d4bee9502139913b745e171504d9c8564
Leonardo Forbici <leonardoforbici1@gmail.com>
2022-04-14 08:05:11 -0300

    configuração de doc.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl:'https://apirest-venda.herokuapp.com'
+  apiUrl:'https://apirest-venda.herokuapp.com',
+  clientId: 'my-angular-app',
+  clientSecret:'@321',
+  obterTokenUrl:'/oauth/token'
 };
